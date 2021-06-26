1cb9289728faef5a41ded6deeb3a15b67c0fccaa
Barry Johnson <bdotmgmt@gmail.com>
2021-06-26 19:05:10 -0400

    Create login route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const AuthController = require('../controllers/authController');
 
 router.post('/signup', AuthController.newUserSignup);
+router.post('/login', AuthController.loginUser);
 
 module.exports = router;
