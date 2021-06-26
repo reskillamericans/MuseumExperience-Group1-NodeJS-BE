7b1ad93d7606574b03f386ceabf29481eb05768f
Whitney Harper <whitneyharper1@aol.com>
2021-06-25 21:45:44 -0400

    Created function that sends email with params

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,21 +1,29 @@
 require("dotenv").config({ path: "../../.env" });
 const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
+    auth: {
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD
+    }
+});
 
-const sendEmail = async ({from = SENDER_ADDRESS, to, subject, message}) => {
-    const transporter = nodemailer.createTransport({
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        auth: {
-            user: process.env.MAIL_USERNAME,
-            pass: process.env.MAIL_PASSWORD
-        }
-    });
-    await transporter.sendMail({from, to, subject, message}, (error, data) => {
+const sendEmail = function (to, subject, subject, message, from) {
+    transporter.sendMail({
+    from: process.env.SENDER_ADDRESS, 
+    to: to,
+    subject: subject,
+    message: message
+    }, (error, data) => {
         if(error) {
             console.log(error);
         } else {
             console.log('Email sent!');
         }
-    });
+    })
 };
+
+
+
