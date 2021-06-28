c860615ed88da6ca082d2d8a997d43626fa80ef5
Collins Park <normanpark85@outlook.com>
2021-06-28 17:02:40 +0300

    env variable connection

--- src/database/setup.js
+++ src/database/setup.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const connectionString = process.env.DATABASE_URL;
 module.exports = function () {
-  mongoose.connect(process.env.DATABASE_URL, {
+  mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
