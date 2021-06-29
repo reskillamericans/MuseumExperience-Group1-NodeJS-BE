04f045c2f97fde9d5c9704b7598f2f003c0d85a7
Collins Park <normanpark85@outlook.com>
2021-06-30 01:42:44 +0300

    solved merge conflicts

--- src/index.js
+++ src/index.js
@@ -4,14 +4,43 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const Question = require('./models/questions');
-const questionsRoutes = require('./routes/questionsRoute')
 const AppError = require('./AppError');
-const dbSetup = require('./database/setup');
-dbSetup();
-app.use(express.json());
-app.use(questionsRoutes)
 app.use(express.urlencoded({extended: true}))
 app.set('views', path.join(__dirname, 'views'));
+
+const { exhibitRoutes } = require("./routes/exhibitRoutes");
+const questionsRoutes = require('./routes/questionsRoute')
+const authRoutes = require('./routes/authRoutes');
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//==================================================
+// DATABASE
+//==================================================
+const dbSetup = require("./database/setup");
+
+dbSetup();
+
+//==================================================
+// Routes
+//==================================================
+app.use(exhibitRoutes);
+app.use('/auth', authRoutes);
+app.use(questionsRoutes);
+
+//==================================================
+// Seeders
+//==================================================
+
+const { importData } = require("./seeders/exhibitsSeeder");
+console.log(importData());
+
+//Placeholder routes for webpages
+app.get("/", (req, res) => {
+  res.send("Welcome to Museum App");
+});
 
 //Server
 app.listen(port, () => {
