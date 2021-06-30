d2c5b823e49a1496e772112af6beba06881deb57
Barry Johnson <bdotmgmt@gmail.com>
2021-06-30 00:13:15 -0400

    Create exhibit subscription/subscription cancellation controllers

---
+++ src/controllers/subscriptionController.js
@@ -0,0 +1,12 @@
+const { SubscriptionModel } = require("./../models/subscription");
+
+// Exhibit subscription controller
+exports.subscribeToExhibit = (req, res) => {
+    return null;
+}
+
+
+// Subscription cancellation contoller
+exports.cancelSubscriptionToExhibit = (req, res) => {
+    return null;
+}
