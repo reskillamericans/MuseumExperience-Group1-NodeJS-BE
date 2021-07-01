30e2ad40070d0f5ca81a12ff4c202edf2646c199
Barry Johnson <bdotmgmt@gmail.com>
2021-07-01 19:55:30 -0400

    Push new subscription to subscriptions array in user schema

--- src/controllers/subscriptionController.js
+++ src/controllers/subscriptionController.js
@@ -1,17 +1,30 @@
 const Subscription = require("./../models/subscription");
+const User = require("./../models/users")
+const Exhibit = require('./../models/exhibit');
 
 // Exhibit subscription controller
 exports.subscribeToExhibit = (req, res) => {
-    req.body.userId = req.user.id
     Subscription.create({
-        userId: req.user.id,
-        exhibitId: req.body.exhibitId,
+        user: req.user.id,
+        exhibit: req.body.exhibit,
         status: req.body.status
     }, (err, newSubscription) => {
         if (err) {
             return res.status(500).json({ message: err });
         } else {
-            return res.status(200).json({ message: "User successfully subscribed to exhibit!", newSubscription })
+            let conditions = {};
+            if (req.query.id) {
+                conditions.id = req.query.id;
+            }
+            User.findById(req.user.id, (err, user) => {
+                if (err) {
+                    return res.status(500).json({ message: err });
+                } else {
+                    user.subscriptions.push(newSubscription.exhibit);
+                    console.log(user);
+                    return res.status(200).json({ message: "User successfully subscribed to exhibit!", newSubscription }); 
+                }
+            })
         }
     })
 }
--- src/routes/subscriptionRoutes.js
+++ src/routes/subscriptionRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateUser } = require('../middlewares/authentication');
-const { subscribeToExhibit } = require('../controllers/subscriptionController');
+const { subscribeToExhibit, cancelSubscriptionToExhibit } = require('../controllers/subscriptionController');
 
 router.post('/subscribe/exhibit/:id', authenticateUser, subscribeToExhibit);
+router.put('/cancel-subscription/:id', authenticateUser, cancelSubscriptionToExhibit);
 
 exports.subscriptionRoutes = router;
