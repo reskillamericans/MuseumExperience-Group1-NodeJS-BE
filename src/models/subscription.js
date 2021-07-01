38a8551c0ecacc8c310c35e2d0479b215862a54a
Barry Johnson <bdotmgmt@gmail.com>
2021-07-01 17:41:45 -0400

    Refactor user and exhibit fields in subscription schema

--- src/models/subscription.js
+++ src/models/subscription.js
@@ -2,18 +2,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const subscriptionSchema = new Schema({
-    userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    exhibitId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Exhibit'
-    },
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    exhibit: { type: Schema.Types.ObjectId, ref: 'Exhibit' },
     status: {
         type: String,
         enum:['active', 'cancelled']
-    }
+    },
 });
 
 module.exports = mongoose.model('Subscription', subscriptionSchema);
