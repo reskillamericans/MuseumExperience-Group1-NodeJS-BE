a82d01b1f06d6d054b7d0c35383f7a4287cc7c91
Barry Johnson <bdotmgmt@gmail.com>
2021-07-01 17:36:04 -0400

    Refactor user schema to include field for subscriptions

--- src/models/users.js
+++ src/models/users.js
@@ -22,7 +22,8 @@ const userSchema = new Schema({
     dateOfBirth: {
         type: Date,
         required: true
-    }
+    },
+    subscriptions: [{ type: Schema.Types.ObjectId, ref: 'Subscription' }]
 });
 
 module.exports = mongoose.model('User', userSchema);
