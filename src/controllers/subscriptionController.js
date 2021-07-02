29decf590ee095b7d2b10cd20342e9581f025956
Barry Johnson <bdotmgmt@gmail.com>
2021-07-01 20:06:18 -0400

    Refactor subscription cancellation controller

--- src/controllers/subscriptionController.js
+++ src/controllers/subscriptionController.js
@@ -32,5 +32,22 @@ exports.subscribeToExhibit = (req, res) => {
 
 // Subscription cancellation contoller
 exports.cancelSubscriptionToExhibit = (req, res) => {
+    Subscription.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, subscription) => {
+        if (err) {
+            return res.status(500).json({message: err});
+        } else if (!subscription) {
+            return res.status(404).json({message: 'Subscription not found'});
+        } else {
+            subscription.save((err, savedSubscription) => {
+                if (err) {
+                    return res.status(400).json({message: err});
+                } else if (subscription.status === "cancelled") {
+                    return res.status(401).json({ message: "Subscription is already cancelled"})
+                } else {
+                    return res.status(200).json({message: 'Subscription successfully cancelled!', subscription});
+                }
+            });
+        }
+    })
     return null;
 }
