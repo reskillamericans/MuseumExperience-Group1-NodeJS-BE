dd3b0263309a4e422375cb793f7a5c6ff60af25a
Collins Park <normanpark85@outlook.com>
2021-07-09 00:46:36 +0300

    added comments controller

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -6,9 +6,7 @@ exports.createComment = async (req, res, next) => {
     try {
         const { title, description } = req.body;
         const comment = new Comment({ title, description });
-        if (comment.length === 0) {
-            throw new AppError('Sorry, comment cannot be blank', 401);
-        }
+        if (comment.length === 0) throw new AppError('Sorry, comment cannot be blank', 401);
         await comment.save();
         const deliverCommentTo = process.env.ADMIN_ADDRESS;
         sendEmail(deliverCommentTo, title, description, process.env.SENDER_ADDRESS);
