ca61100867768b1ff0c31dec6d55ff22c86c9334
Norman Park <normanpark85@outlook.com>
2021-07-11 22:58:49 +0300

    fixed conflict

--- src/controllers/commentsController.js
+++ src/controllers/commentsController.js
@@ -7,7 +7,7 @@ exports.createComment = async (req, res, next) => {
     try {
         const { text } = req.body;
         const comment = new Comment({ text });
-        if (comment.length === 0) throw new AppError('Sorry, comment cannot be blank', 401);
+        if (!text) {throw new AppError('Sorry, comment cannot be blank', 401);}
         await comment.save();
         const deliverCommentTo = process.env.ADMIN_ADDRESS;
         sendEmail(deliverCommentTo, text, process.env.SENDER_ADDRESS);
