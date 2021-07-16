82c5f55838f0caaf48e0f7d392861760537ccd20
TheycallmeNate <nathanmangal@gmail.com>
2021-07-16 08:46:01 -0400

    edits to make app work as expected to write the docs

--- src/controllers/questionsController.js
+++ src/controllers/questionsController.js
@@ -1,41 +1,40 @@
-const Question = require('../models/questions');
-const AppError = require('../AppError');
-const sendEmail = require('../services/emailService');
+const Question = require("../models/questions");
+const AppError = require("../AppError");
+const sendEmail = require("../services/emailService");
 
 exports.fetchQuestions = async (req, res, next) => {
-    try {
-        const questions = await Question.find({});
-        if (!questions) {
-            throw new AppError('Questions not found', 404);
-        }
-        return res.status(200).json({ questions });
-    } catch (err) {
-        next(err)
+  try {
+    const questions = await Question.find({});
+    if (!questions) {
+      throw new AppError("Questions not found", 404);
     }
+    return res.status(200).json({ questions });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.createQuestion = async (req, res, next) => {
-    try {
-        const { title, description } = req.body;
-        const question = new Question(req.body);
-        await question.save();
-        const deliverQuestionTo = process.env.ADMIN_ADDRESS;
-        sendEmail(deliverQuestionTo, title, description, process.env.SENDER_ADDRESS);
-        return res.status(200).json({ question })
-      }catch (err) {
-        next(err)
-    }
+  try {
+    const { title, description } = req.body;
+    const question = new Question(req.body);
+    await question.save();
+    const deliverQuestionTo = process.env.ADMIN_ADDRESS;
+    sendEmail(deliverQuestionTo, title, description, process.env.SENDER_ADDRESS);
+    return res.status(200).json({ question });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.fetchSingleQuestion = async (req, res, next) => {
-    try {
-        const question = await Question.findById(req.params.id);
-        if (!question) {
-            throw new AppError('Question not found', 404);
-        }
-        return res.status(200).json({ question })
-    } catch (err) {
-        next(err)
+  try {
+    const question = await Question.findById(req.params.id);
+    if (!question) {
+      throw new AppError("Question not found", 404);
     }
+    return res.status(200).json({ question });
+  } catch (err) {
+    next(err);
+  }
 };
-
